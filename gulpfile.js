14b14c12889aee1cfd3c2a2d6dda67a902217290
dimacorporal1901 <abshilava2000@yandex.ru>
2024-08-09 17:47:16 +0300

    Работа над скриптом

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,23 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import svgo from 'gulp-svgmin';
+import squoosh from 'gulp-libsquoosh';
+import terser from 'gulp-terser';
+import rename from 'gulp-rename';
+import svgstore from 'gulp-svgstore';
+import csso from 'postcss-csso';
+import del from 'del';
+return gulp.src('source/less/style.less', { sourcemaps: true })
+.pipe(plumber())
+.pipe(less())
+.pipe(postcss([
+  autoprefixer(),
+  csso()
+]))
+.pipe(rename('style.min.css'))
+.pipe(gulp.dest('build/css', { sourcemaps: ' . ' }))
+.pipe(browser.stream());
 
 // Styles
 
@@ -16,6 +33,62 @@ export const styles = () => {
     ]))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browser.stream());
+}
+
+// Images
+
+const images = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'));
+}
+
+const copyimages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(gulp.dest('build/img'));
+}
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/img'));
+}
+
+// SVG
+
+const SVG = () =>
+  gulp.src('source/img/*.svg')
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+
+const sprite = () => {
+  return gulp.src('source/img/*.svg')
+    .pipe(svgo())
+    .pipe(svgstore({
+    inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 }
 
 // Server
